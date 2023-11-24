b3cbf1ca5f7217a646062aa5cbea76fb65d27fe7
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-11-23 23:31:11 -0500

    feature(testing): test for register done

--- src/test/java/com/deviot/cropmasterbackend/CropMasterBackendApplicationTests.java
+++ src/test/java/com/deviot/cropmasterbackend/CropMasterBackendApplicationTests.java
@@ -1,13 +1,37 @@
 package com.deviot.cropmasterbackend;
-
+import com.deviot.cropmasterbackend.account.domain.model.commands.CreateAccountCommand;
+import com.deviot.cropmasterbackend.account.domain.model.commands.LogInCommand;
+import com.deviot.cropmasterbackend.account.domain.services.AccountCommandService;
+import com.deviot.cropmasterbackend.account.infrastructure.dtos.AuthResponse;
+import com.deviot.cropmasterbackend.account.infrastructure.repositories.AccountRepository;
+import com.deviot.cropmasterbackend.profile.interfaces.rest.dto.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CropMasterBackendApplicationTests {
 
+	@MockBean
+	private AccountRepository accountRepository;
+
+	private AccountCommandService accountCommandService;
+
 	@Test
-	void contextLoads() {
+	void testRegisterUser() {
+		when(accountRepository.findByEmail(anyString())).thenReturn(null);
+
+		CreateAccountCommand createAccountCommand = new CreateAccountCommand("correo@example.com", "123", "FARMER");
+
+		AuthResponse response = this.accountCommandService.handle(createAccountCommand);
+
+		assertNotNull(response);
+		assertNotNull(response.getToken());
+
 	}
 
 }
